efc9e18e37343c4819425b38781f75928f01c3c5
moeshanti <ajmoeshanti@gmail.com>
2026-04-11 20:28:28 +0300

    chore: update Two Skies 3D model and bump SW cache to v1.4 to force global client refresh

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@
  - Lead AR and Motion Videos Designer: Shalesh Shekhar
 ==============================================================================
 */
-const CACHE_NAME = 'fann-ar-v1.3';
+const CACHE_NAME = 'fann-ar-v1.4';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
