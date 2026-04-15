eb6a9503da18ea7214a74dc071cd29695deffe05
moeshanti <ajmoeshanti@gmail.com>
2026-04-15 22:58:32 +0300

    fix: suppress landscape AR warning during active video playback and bump to v1.14

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@
  - Lead AR and Motion Videos Designer: Shalesh Shekhar
 ==============================================================================
 */
-const CACHE_NAME = 'fann-ar-v1.13';
+const CACHE_NAME = 'fann-ar-v1.14';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
