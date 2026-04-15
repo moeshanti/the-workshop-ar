eeef5408408686f765b038bc7ed98f52517ef9b1
moeshanti <ajmoeshanti@gmail.com>
2026-04-15 16:24:18 +0300

    ui: elevate landscape warning screen with animated rotation cue and glassmorphism

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@
  - Lead AR and Motion Videos Designer: Shalesh Shekhar
 ==============================================================================
 */
-const CACHE_NAME = 'fann-ar-v1.10';
+const CACHE_NAME = 'fann-ar-v1.11';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
